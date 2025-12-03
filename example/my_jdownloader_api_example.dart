a119f20b941236bb5d4a7f7b23e08be7a43ac30f
Adrien Bonnin <adbonnin@users.noreply.github.com>
2025-12-03 20:02:33 +0100

    :tada: Initialize package structure

---
+++ example/my_jdownloader_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:my_jdownloader_api/my_jdownloader_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/my_jdownloader_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:my_jdownloader_api/my_jdownloader_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
